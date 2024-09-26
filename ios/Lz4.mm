f83e540412eb573dd5f4a6e52fe88c457df88cf7
Mateo Guzmán <info@mateoguzman.net>
2024-09-26 11:16:33 +0200

    feat(ios): lz4::getLz4VersionString objective-c bind

--- ios/Lz4.mm
+++ ios/Lz4.mm
@@ -21,6 +21,15 @@ RCT_EXPORT_METHOD(getLz4VersionNumber:(RCTPromiseResolveBlock)resolve
     resolve(result);
 }
 
+RCT_EXPORT_METHOD(getLz4VersionString:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    std::string versionString = lz4::getLz4VersionString();
+    NSString *result = [NSString stringWithUTF8String:versionString.c_str()];
+
+    resolve(result);
+}
+
 // Don't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
