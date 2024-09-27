5fb246a9d8ab5fd3fea211665e286c037e41413f
Mateo Guzmán <info@mateoguzman.net>
2024-09-27 23:28:04 +0200

    style(example): cleaning up example code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -24,16 +24,8 @@ export default function App() {
 
       const imageLibraryResult = await launchImageLibrary(options);
 
-      if (imageLibraryResult.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (imageLibraryResult.errorCode) {
-        console.log('Image Picker Error: ', imageLibraryResult.errorMessage);
-      } else if (
-        imageLibraryResult.assets &&
-        imageLibraryResult.assets.length > 0
-      ) {
+      if (imageLibraryResult.assets && imageLibraryResult.assets.length > 0) {
         const asset = imageLibraryResult.assets[0];
-        console.log({ asset });
 
         if (!asset?.uri) {
           console.log('No asset URI found');
@@ -44,23 +36,11 @@ export default function App() {
         const destinationPath = asset.uri.replace(/(.*)(\..*)/, '$1.lz4');
 
         const compressFileResult = await compressFile(
-          // '/Users/mateoguzman/Library/Developer/CoreSimulator/Devices/8EA59C5A-23FA-4BFD-8159-D8FBA3A46D7B/data/Containers/Data/Application/A4C994AC-7778-4B60-AD74-6766E2CE1283/tmp/44307235-C5D8-4533-98E8-DB2F76502344.jpg',
-          // 'data/user/0/lz4.example/cache/rn_image_picker_lib_temp_859be96a-cfaa-419f-9d48-0e6be30e8d00.png',
           sourcePath,
-          // use the same uri as the source path to overwrite the file and with extension lz4
-          // '/Users/mateoguzman/Library/Developer/CoreSimulator/Devices/8EA59C5A-23FA-4BFD-8159-D8FBA3A46D7B/data/Containers/Data/Application/A4C994AC-7778-4B60-AD74-6766E2CE1283/tmp.lz4'
           destinationPath
         );
 
         console.log({ compressFileResult });
-
-        const decompressFileResult = await decompressFile(
-          destinationPath,
-          // add_dash_decompressed to the end of the file name
-          sourcePath.replace(/(.*)(\..*)/, '$1_decompressed$2')
-        );
-
-        console.log({ decompressFileResult });
       }
     } catch (error) {
       console.log({ error });
@@ -68,35 +48,19 @@ export default function App() {
   };
 
   const executeDecompressFile = async () => {
-    const [file] = await pick({
-      copyTo: 'cachesDirectory',
-    });
+    const [file] = await pick({ copyTo: 'cachesDirectory' });
 
-    console.log({ file });
-
-    if (!file) {
-      console.log('No file selected');
-      return;
-    }
-
-    if (!file.fileCopyUri) {
-      console.log('No source path found');
-      return;
-    }
+    if (!file || !file.fileCopyUri) return;
 
     const sourcePath = file.fileCopyUri;
     const destinationPath = file.fileCopyUri.replace(/(.*)(\..*)/, '$1.lz4');
 
-    const compressFileResult = await compressFile(
-      // '/Users/mateoguzman/Library/Developer/CoreSimulator/Devices/8EA59C5A-23FA-4BFD-8159-D8FBA3A46D7B/data/Containers/Data/Application/A4C994AC-7778-4B60-AD74-6766E2CE1283/tmp/44307235-C5D8-4533-98E8-DB2F76502344.jpg',
-      // 'data/user/0/lz4.example/cache/rn_image_picker_lib_temp_859be96a-cfaa-419f-9d48-0e6be30e8d00.png',
+    const decompressFileResult = await decompressFile(
       sourcePath,
-      // use the same uri as the source path to overwrite the file and with extension lz4
-      // '/Users/mateoguzman/Library/Developer/CoreSimulator/Devices/8EA59C5A-23FA-4BFD-8159-D8FBA3A46D7B/data/Containers/Data/Application/A4C994AC-7778-4B60-AD74-6766E2CE1283/tmp.lz4'
       destinationPath
     );
 
-    console.log({ compressFileResult });
+    console.log({ decompressFileResult });
   };
 
   useEffect(() => {
