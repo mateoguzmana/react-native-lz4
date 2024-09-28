1b232751642ef0f90789dae6a8f5d2698b08ad60
Mateo Guzmán <info@mateoguzman.net>
2024-09-28 20:50:18 +0200

    fix(lib): cleaning up non-needed code

--- ios/Lz4.mm
+++ ios/Lz4.mm
@@ -1,20 +1,13 @@
 #import "Lz4.h"
 #import <React/RCTBridge+Private.h>
-#import <React/RCTUtils.h>
-#import "../cpp/react-native-lz4.h"
 
 using namespace facebook;
 
 @implementation Lz4
+RCT_EXPORT_MODULE(Lz4)
 
 @synthesize bridge = _bridge;
 @synthesize methodQueue = _methodQueue;
-
-RCT_EXPORT_MODULE(Lz4)
-
-+ (BOOL)requiresMainQueueSetup {
-  return YES;
-}
 
 RCT_EXPORT_METHOD(getLz4VersionNumber:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
@@ -56,9 +49,9 @@ RCT_EXPORT_METHOD(decompressFile:(NSString *)sourcePath
 
 RCT_EXPORT_METHOD(initializeLz4)
 {
-  NSLog(@"Binding Lz4 to JSI");
   RCTBridge *bridge = [RCTBridge currentBridge];
   RCTCxxBridge *cxxBridge = (RCTCxxBridge *) bridge;
+
   if (cxxBridge == nil) {
     NSLog(@"CxxBridge is nil");
   }
